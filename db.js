4725a46ec2ed703d23c0635af23819c0128051fd
JimenaCastellanos <jimena.castellanos.e@lasallenoroeste.edu.mx>
2025-05-04 15:40:25 -0700

    db.js

--- db.js
+++ db.js
@@ -5,7 +5,10 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
-  }
+  },
 });
 
+console.log('🔗 Conectando a base de datos:', process.env.DATABASE_URL);
+
 module.exports = pool;
+
